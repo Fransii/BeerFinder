168ad7cc840f51604ee7b4b2348cd00d57cdd31c
Fransii <fransiid@gmail.com>
2016-11-13 19:37:10 +0100

    MainPageSevlet
    Servlet for main page

---
+++ src/com/beerfinder/Main/MainPageServlet.java
@@ -0,0 +1,24 @@
+package com.beerfinder.Main;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by franciszekdanes on 13.11.2016.
+ */
+@WebServlet(name = "MainPageServlet", value = {"/MainPage"})
+public class MainPageServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+
+        PrintWriter out = response.getWriter();
+        out.println("Main Page");
+
+    }
+}
